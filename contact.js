02c37c8c13767718d39e1dff02073fd71807060e
misha <76406959+mistahanish@users.noreply.github.com>
2026-04-21 17:36:28 -0400

    fixed contact card

---
+++ contact.js
@@ -0,0 +1,18 @@
+const CONTACT = [
+  {
+    label: "email",
+    href: "mailto:me@hanishv.com",
+  },
+  {
+    label: "instagram",
+    href: "https://instagram.com/hanishh_15",
+  },
+  {
+    label: "linkedin",
+    href: "https://www.linkedin.com/in/hanish-vadlamudi/",
+  },
+  {
+    label: "github",
+    href: "https://github.com/mistahanish",
+  },
+];
